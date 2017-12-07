dcffb91ef07f875f5b572fdac25f52545df4d4c4
Bernard ng <ngandubernard@gmail.com>
2017-12-07 10:21:42 +0200

    suppression des packages inutiles

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,8 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Twig_Extensions_' => array($vendorDir . '/twig/extensions/lib'),
-    'Twig_' => array($vendorDir . '/twig/twig/lib'),
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
-    'Pagerfanta\\' => array($vendorDir . '/pagerfanta/pagerfanta/src'),
 );
