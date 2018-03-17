0e3f164efb6c8c3df7b2e105fd3a97bd9b47534a
Bernard ng <ngandubernard@gmail.com>
2018-03-17 03:40:35 +0200

    Design start

--- src/Ngpictures/Views/includes/default-slider.php
+++ src/Ngpictures/Views/includes/default-slider.php
@@ -1,59 +1,40 @@
-<!--========================= SLIDER ========================-->
-<div class="slider slider-no-pad">
+<div class="slider">
     <ul class="slides">
         <li>
-            <span class="ng-cover"></span>
-            <img src="/imgs/banner-wd.jpeg">
+            <img src="/imgs/bg.jpg">
             <div class="caption center-align">
                 <h3 class="ng-heading">Bienvenue sur Ngpictures</h3>
                 <h6 class="light grey-text text-lighten-3">
                 Découvrez la version 2.0 de ngpictures et toutes les nouvelles fonctionnalités.</h6>
-                 <br>
-                <a href="/about/us">
-                    <button class="link-btn">en savoir plus</button>
-                </a>
             </div>
+            <div class="bg"></div>
         </li>
         <li>
-            <span class="ng-cover"></span>
-            <img src="/imgs/bg/share-bg.jpg">
+            <img src="/imgs/bg2.jpg">
             <div class="caption center-align">
                 <h3 class="ng-heading">Partager vos Photos</h3>
                 <h6 class="light grey-text text-lighten-3">
                 Notre galerie grandit grâce à vos photos, n'oubliez pas d'en poster.</h6>
-                <br>
-                <a href="/gallery/post">
-                    <button class="link-btn">poster une photo</button>
-                </a>
             </div>
+            <div class="bg"></div>
         </li>
         <li>
-            <span class="ng-cover"></span>
-            <img src="/imgs/bg/discover-bg.jpg">
+            <img src="/imgs/bg3.jpg">
             <div class="caption center-align">
-                <h3 class="ng-heading">Deep shooting</h3>
+                <h3 class="ng-heading">Deep Shooting</h3>
                 <h6 class="light grey-text text-lighten-3">
                 Faites des shootings photos comme jamais avant, où vous voulez, quand vous voulez</h6>
-                <br>
-                <a href="/about/shooting">
-                    <button class="link-btn">en savoir plus</button>
-                </a>
             </div>
+            <div class="bg"></div>
         </li>
         <li>
-            <span class="ng-cover"></span>
-            <img src="/imgs/bg/dance-bg.jpg">
+            <img src="/imgs/bg4.jpg">
             <div class="caption center-align">
-                <h3 class="ng-heading">Wonderful DP</h3>
+                <h3 class="ng-heading">Discover</h3>
                 <h6 class="light grey-text text-lighten-3">
                 Wonderful design and photography, faites un tour dans la galerie et voyez par vous-même.</h6>
-                <br>
-                <a href="/gallery">
-                    <button class="link-btn">voir la gallerie</button>
-                </a>
             </div>
+            <div class="bg"></div>
         </li>
     </ul>
 </div>
-
-
--- src/Ngpictures/Views/includes/menu.php
+++ src/Ngpictures/Views/includes/menu.php
@@ -1,52 +1,58 @@
-<!--=============== NAVBAR ===================-->
 <header>
-    <div class="navbar-fixed z-depth-2">
-        <nav class="ng-menu-reset">
-            <div class="nav-wrapper ng-menu shrink" id="menu">
-                <div class="row container">
-                    <a href="/home" class="brand-logo left ng-menu-title">
-                        Ngpictures
-                    </a>
-                    <a href="#" data-activates="mobile-side-nav" class="button-collapse btn primary-c right">
-                        <i class="social social-menu"></i>
-                    </a>
-                    <ul class="right hide-on-med-and-down links">
-                        <span id="menu-item-active" data-isActive="<?= $pageManager::getActivePage() ?>"></span>
-                        <li>
-                            <form action="/search" method="get">
-                                <input class="default-form" type="search" name="q" id="q">
+    <div class="navbar-fixed">
+        <nav class="shadow-3">
+            <div class="nav-wrapper container " id="menu">
+                <a href="/" class="brand-logo">
+                    <img src="/imgs/logo-white.png" class="logo" alt="logo">
+                </a>
+                <a href="#" data-activates="mobile-menu" class="btn blue-grey dark-3 button-collapse">
+                    <i class="icon icon icon-th"></i>
+                </a>
+                <ul class="right hide-on-med-and-down">
+                    <span id="menu-item-active" data-isActive="<?= $pageManager::getActivePage() ?>"></span>
+
+                    <li class="search">
+                        <div class="search">
+                            <form action="/search" method="get" class="ui icon input">
+                                <input name="q" type="text" placeholder="Recherches...">
+                                <i class="icon icon-search"></i>
                                 <input type="submit" value="send">
                             </form>
+                        </div>
+                    </li>
+
+                    <?php if ($activeUser && $activeUser->rank == "admin") : ?>
+                        <li><a href="<?= ADMIN ?>"> <i class="icon icon-code"></i></a></li>
+                    <?php endif; ?>
+
+                    <?php if ($activeUser) : ?>
+                        <li id="La Communauté"><a href="/community">Communauté</a></li>
+                    <?php endif; ?>
+
+                    <li id="blog"><a href="/blog"> Blog</a></li>
+                    <li id="posts"><a href="/posts"> Actualités</a></li>
+                    <li id="Gallerie"><a href="/gallery"> Gallerie</a></li>
+
+                    <?php if ($activeUser) : ?>
+                        <li class="user-actions">
+                            <a href="#" class="dropdown-button" data-activates="user-dropdown">
+                                <img src="<?= $activeUser->avatarUrl; ?>" class="user" width="50" height="50">
+                            </a>
                         </li>
-                        <?php if ($activeUser && $activeUser->rank == "admin") : ?>
-                            <li><a href="<?= ADMIN ?>" class="ng-menu-item"> Admin</a></li>
-                        <?php endif; ?>
-
-                        <li id="Accueil"><a href="/home" class="ng-menu-item">Accueil</a></li>
-                        <?php if ($activeUser) : ?>
-                            <li id="La Communauté"><a href="/community" class="ng-menu-item">Communauté</a></li>
-                        <?php endif; ?>
-
-
-                        <li id="blog"><a href="/blog" class="ng-menu-item"> Blog</a></li>
-                        <li id="posts"><a href="/posts" class="ng-menu-item"> Actualités</a></li>
-                        <li id="Gallerie"><a href="/gallery" class="ng-menu-item"> Gallerie</a></li>
-                        <?php if ($activeUser) : ?>
-                            <li>
-                                <a href="<?= $activeUser->accountUrl; ?>">
-                                    <img src="<?= $activeUser->avatarUrl; ?>" class="ng-menu-avatar" width="50" height="50">
-                                </a>
-                            </li>
-                        <?php endif; ?>
-
-                        <?php if (!$activeUser) : ?>
-                            <li><a href="/sign" class="link-btn">Inscription</a></li>
-                            <li><a href="/login" class="link-btn">Connexion</a></li>
-                        <?php endif; ?>
-
-                    </ul>
-                </div>
+                    <?php else: ?>
+                        <li class="action"><a href="/sign" class="btn blue-grey waves-effect dark-3">Inscription</a></li>
+                        <li class="action"><a href="/login" class="btn blue-grey waves-effect dark-3">Connexion</a></li>
+                    <?php endif; ?>
+                </ul>
             </div>
         </nav>
     </div>
+    <ul id="user-dropdown" class="dropdown-content">
+        <li><a href="<?= $activeUser->accountUrl; ?>">Profile</a></li>
+        <li><a href="<?= $activeUser->editUrl; ?>">Editer le Profile</a></li>
+        <li><a href="<?= $activeUser->postUrl; ?>">Poster Une photo</a></li>
+        <li><a href="<?= $activeUser->postUrl; ?>">Mes Publications</a></li>
+        <li><a href="<?= $activeUser->followingUrl; ?>">Mes abonnements</a></li>
+        <li><a href="/logout">Déconnexion</a></li>
+    </ul>
 </header>
