44bc3be018024e524966f889e653caeeea51cb7d
bernard-ng <ngandubernard@gmail.com>
2018-04-09 00:12:15 +0200

    Delete sounds files and set Database

--- src/Ngpictures/Views/includes/flash.php
+++ src/Ngpictures/Views/includes/flash.php
@@ -1,10 +1,5 @@
 <?php if ($flashMessageManager->has()) : ?>
     <?php foreach ($flashMessageManager->get() as $type => $message) : ?>
-    <audio autoplay>
-         <source src="/sounds/flash-message.m4r" type="audio/m4r; codecs=opus"/>
-         <source src="/sounds/flash-message.ogg" type="audio/ogg; codecs=vorbis"/>
-         <source src="/sounds/flash-message.mp3" type="audio/mpeg"/>
-    </audio>
         <script>
             var message = "<?php echo $message ?>";
             var type = "<?php echo $type ?>";
