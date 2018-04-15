951f054996f8ab47334dcd0d6bdd0def69cc8d1d
bernard-ng <ngandubernard@gmail.com>
2018-04-15 16:32:22 +0200

    working on user profile

--- src/Ngpictures/Views/front_end/users/account/account.php
+++ src/Ngpictures/Views/front_end/users/account/account.php
@@ -1,66 +1,44 @@
-<section class="row container">
-
 <style type="text/css">
 
-
-
-
-
-
-.tag-label {
-    background: rgb(85, 85, 85);
-    padding: 2px 5px;
-    border-radius: 3px;
-    color: rgb(255, 255, 255);
-    font-size: 12px;
-    text-decoration: none;
-    margin-left: 5px;
-    vertical-align: middle;
-    display: inline-block;
-}
-
-.profile-header {
-
-}
-.profile-header__img {
-    background: rgb(238, 238, 238);
-    border-radius: 50%;
-    width: 100px;
-    height: 100px;
+.profile-img {
+    background: #000;
+    border-radius: 100%;
+    width: 120px;
+    height: 120px;
     overflow: hidden;
     margin: 10px;
     vertical-align: middle;
     display: inline-block;
 }
-.profile-header__content {
+.profile-content {
     width: calc(100% - 200px);
     vertical-align: middle;
     display: inline-block;
     position: relative;
     padding: 20px 5px 20px 5px;
 }
-.profile-header__title {
+.profile-title {
     margin: 0px;
 }
-.profile-header__subtitle {
+.profile-subtitle {
     margin: 0px;
 }
-.profile-header__title {
+.profile-title {
     font-size: 2.2em;
     font-weight: 600;
-    font-family: robot-title
+    text-transform: capitalize;
 }
-.profile-header__subtitle {
+.profile-subtitle {
     margin: 5px 0px;
-    color: #00695c;
     font-size: 14px;
+    font-weight: 300;
     display: block;
 }
-.profile-header__links {
-    color: rgb(85, 85, 85);
+.profile-links {
+    color: #428bca;
     font-size: 14px;
 }
-.profile-header__link {
+.profile-link {
     color: inherit;
     text-decoration: none;
     margin-right: 10px;
@@ -68,22 +46,19 @@
 }
 
 
-.profile-header__actions {
+.profile-actions {
     top: 50%;
     text-align: right;
     right: -15px;
     margin-top: -20px;
     position: absolute;
 }
-.profile-header__action {
+.profile-action {
     padding: 0px;
     width: 60px;
     height: 40px;
     line-height: 40px;
     margin-left: 10px;
-}
-.profile-header__facebook {
-    background: rgb(59, 89, 152);
 }
 
 .user-position{
@@ -92,108 +67,52 @@
 }
 
 .bio{
-    margin: -10px 20px 10px 20px;
-    border: 2px solid #ccc;
+    margin: 10px 20px 10px 20px;
+    border-top: 1px solid rgba(244,244,244,0.4);
     padding: 20px !important;
-    border-radius: 3px;
     position: relative;
 }
-.bio:after{
-    content: '';
-    border: 15px solid #ccc;
-    border-radius: 8px;
-    border-right-color: transparent;
-    border-left-color: transparent;
-    border-top-color: transparent;
-    position: absolute;
-    bottom: 100%;
-    left: 40px;
-}
-
 
 
 </style>
-<div class="card col l12 s12 m12 profil-card">
-    <img class="profile-header__img" alt="<?= $user->name; ?>"  src="<?= $user->avatarUrl ?>">
-    <div class="profile-header__content row">
-        <div class="row col lg6 m12">
-        <h1 class="profile-header__title">
-            <?= $user->name ?>
-        </h1>
-        <h2 class="profile-header__subtitle"><?= $user->email ?></h2>
+<div class="jumbotron-user">
+    <div class="container">
+        <img class="profile-img " alt="<?= $user->name; ?>"  src="<?= $user->avatarUrl ?>">
+        <div class="profile-content row">
+            <div class="row col lg6 m12">
+            <h1 class="profile-title">
+                <?= $user->name ?>
+            </h1>
+            <h2 class="profile-subtitle">
+                <?= $user->email ?>
+            </h2>
 
-        <?php if ($activeUser && $activeUser->id == $user->id) : ?>
-            <div class="profile-header__links">
-                <a class="profile-header__link" href="<?= $user->editUrl ?>">
-                    <i class="icon icon-edit"></i>&nbsp;Editer le profil
-                </a>
-                <a class="profile-header__link" href="/logout/">
-                    <i class="icon icon-off"></i>&nbsp;deconnexion
-                </a>
-            </div>
-        <?php endif; ?>
-
-        </div>
-        <div class="hide-on-med-and-down profile-header__actions">
-            <a class="profile-header__actions  btn primary-c right" title="Suivre cette personne" href="/leaderboard/">
-                <i class="icon icon-plus"></i>
-            </a>
-            <a href="#" class="profile-header__actions profile-header__facebook btn" rel="nofollow" target="_blank" title="partager sur facebook">
-                <i class="social social-facebook-1"></i>
-            </a>
-        </div>
-    </div>
-
-    <div class="col l12 m12 s12 bio">
-        <?= $user->bio ?>
-    </div>
-</div>
-<?php include(APP."/Views/includes/left-aside.php"); ?>
-<div class="col l9 s12 m12 ">
-    <?php if ($activeUser && $activeUser->id == $user->id) : ?>
-        <nav class="nav z-depth-2">
-            <div class="nav-wrapper">
-                <ul>
-                    <li><a href="<?= $user->addPostUrl ?>">Poster</a></li>
-                    <li class="right"><a href="<?= $user->postsUrl;  ?>">Mes publications</a></li>
-                    <li class="right"><a href="<?= $user->followersUrl; ?>">Mes abonnés</a></li>
-                    <li class="right"><a href="<?= $user->followingsUrl; ?>">Mes abonnements</a></li>
-                </ul>
-            </div>
-        </nav>
-    <?php endif; ?>
-    <div class="col l12 m12 s12">
-        <div class="card col s12">
-            <section id="gallery" style="margin: 0 auto;">
-            <?php if (!empty($posts)) : ?>
-            <?php foreach ($posts as $a) : ?>
-                <a class="photo" href="<?= $a->thumbUrl ?>" alt="<?= $a->title ?>">
-                    <span class="photo-title"><?= $a->title ?></span>
-                    <span class="photo-bg"></span>
-                    <img src="<?= $a->thumbUrl ?>" width="60" class="galery-item materialbox"/>
-                </a>
-            <?php endforeach; ?>
-            <?php else : ?>
-                <div class="card">
-                    <div class="no-publication">
-                        <div class="ng-cover"></div>
-                        <p><i class="icon icon-picture"></i> &nbsp;aucune publication pour l'instant</p>
-                    </div>
+            <?php if ($activeUser && $activeUser->id == $user->id) : ?>
+                <div class="profile-links">
+                    <a class="profile-link" href="<?= $user->editUrl ?>">
+                        <i class="icon icon-edit"></i>&nbsp;Editer le profil
+                    </a>
+                    <a class="profile-link" href="<?= $user->editUrl ?>">
+                        <i class="icon icon-edit"></i>&nbsp;Paramètres
+                    </a>
                 </div>
             <?php endif; ?>
+
             </div>
+            <?php if($activeUser && $activeUser->id != $user->id): ?>
+                <div class="hide-on-med-and-down profile-actions">
+                    <a class="profile-actions  btn blue-grey dark-2 right" title="Suivre cette personne" href="<?= $user->followingUrl; ?>">
+                        Suivre
+                    </a>
+                </div>
+            <?php endif; ?>
         </div>
+
+        <div class="col l12 m12 s12 bio">
+            <?= $user->bio ?>
         </div>
     </div>
 </div>
-
-<script src="/assets/js/lib/jquery.min.js"></script>
-<script src="/assets/js/lib/masonry.js"></script>
-<script type="text/javascript">
-
-$("#gallery").masonry({
-    itemSelector: ".photo",
-    isAnimated: true,
-    isFitWidth: true
-});
-</script>
+<ul class="tabs transparent">
+        <li class="tab"><a href="">test</a></li>
+    </ul>
