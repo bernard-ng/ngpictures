dae51d4a95e1ea29d60b2311f015d45e22ce6855
bernard-ng <ngandubernard@gmail.com>
2019-01-29 10:22:21 +0200

    [Update] lint to editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path')
 
 module.exports = {
-    entry: './resources/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public/assets/js'),
-        filename: 'app.js'
-    },
-    mode: 'development',
-    watch: true,
-    module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-            }
-        }]
-    }
+  entry: './resources/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/assets/js'),
+    filename: 'app.js'
+  },
+  mode: 'development',
+  watch: true,
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+      }
+    }]
+  }
 }
