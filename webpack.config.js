3827c771f9a041ec80068d85e21c4c4c6de8e92b
bernard-ng <ngandubernard@gmail.com>
2019-01-29 10:04:34 +0200

    [Update] add mode and modules keys

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ module.exports = {
     entry: './resources/js/index.js',
     output: {
         path: path.resolve(__dirname, 'public/assets/js'),
-        file: 'app.js'
+        filename: 'app.js'
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }]
     }
 }
