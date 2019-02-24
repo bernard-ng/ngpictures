02550f52306516eaeef2144ca5dc8529c272772c
bernard-ng <ngandubernard@gmail.com>
2019-02-24 02:05:24 +0200

    [Update] enable production mode

--- config/constant.php
+++ config/constant.php
@@ -17,7 +17,7 @@ define("FLASH_MESSAGE_KEY", "flash");
 define("TOKEN_KEY", "token");
 define("CAPTCHA_KEY", "captcha");
 define("COOKIE_REMEMBER_KEY", "ngpictures_");
-define('ENV', 'development');
+define('ENV', 'production');
 define('CACHE_BUSTING', false);
 
 //APPLICATION API KEYS
