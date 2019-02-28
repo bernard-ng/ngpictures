16e72beee158708386152627eada3a8ef227d88e
bernard-ng <ngandubernard@gmail.com>
2019-02-28 20:34:34 +0200

    [Update] use function to clean global namespace

--- config/routes/backend.php
+++ config/routes/backend.php
@@ -11,52 +11,54 @@ use Application\Controllers\Admin\UsersController;
 use Application\Controllers\AdminController;
 
 
-/** @var \Framework\Router\Router $router */
-$router->get(ADMIN, [AdminController::class], "admin.index");
-$router->any(ADMIN . "/reports", [ReportsController::class], 'admin.reports.index');
+return function (\Framework\Router\Router $router) {
+    /** @var \Framework\Router\Router $router */
+    $router->get(ADMIN, [AdminController::class], "admin.index");
+    $router->any(ADMIN . "/reports", [ReportsController::class], 'admin.reports.index');
 
-logs_routes : {
-    $router->get(ADMIN . "/logs", [LogsController::class], "admin.logs");
-    $router->get(ADMIN . "/logs/delete", [LogsController::class, 'clear'], "admin.deleteLogs");
-    $router->get(ADMIN . "/logs/send", [LogsController::class, 'send'], "admin.sendLogs");
-}
+    logs_routes : {
+        $router->get(ADMIN . "/logs", [LogsController::class], "admin.logs");
+        $router->get(ADMIN . "/logs/delete", [LogsController::class, 'clear'], "admin.deleteLogs");
+        $router->get(ADMIN . "/logs/send", [LogsController::class, 'send'], "admin.sendLogs");
+    }
 
-pages_routes : {
-    $router->any(ADMIN . "/pages", [PagesEditorController::class, 'show'], "admin.showPages");
-    $router->any(ADMIN . "/pages/:name", [PagesEditorController::class, 'edit'], "admin.editPages");
-}
+    pages_routes : {
+        $router->any(ADMIN . "/pages", [PagesEditorController::class, 'show'], "admin.showPages");
+        $router->any(ADMIN . "/pages/:name", [PagesEditorController::class, 'edit'], "admin.editPages");
+    }
 
-posts_routes : {
-    $router->any(ADMIN . "/blog/categories/add", [CategoriesController::class, 'add'], "admin.categories-add");
-    $router->any(ADMIN . "/blog/categories/edit/:id", [CategoriesController::class, 'edit'], "admin.categories-edit");
-    $router->get(ADMIN . "/blog/categories", [CategoriesController::class], "admin.categories");
-    $router->get(ADMIN . "/confirm/:t/:id", [AdminController::class, 'confirm'], "admin.confirm");
-    $router->get(ADMIN . "/posts", [\Application\Controllers\Admin\PostsController::class], "admin.posts");
-    $router->post(ADMIN . "/delete", [AdminController::class, 'delete'], "admin.delete");
-}
+    posts_routes : {
+        $router->any(ADMIN . "/blog/categories/add", [CategoriesController::class, 'add'], "admin.categories-add");
+        $router->any(ADMIN . "/blog/categories/edit/:id", [CategoriesController::class, 'edit'], "admin.categories-edit");
+        $router->get(ADMIN . "/blog/categories", [CategoriesController::class], "admin.categories");
+        $router->get(ADMIN . "/confirm/:t/:id", [AdminController::class, 'confirm'], "admin.confirm");
+        $router->get(ADMIN . "/posts", [\Application\Controllers\Admin\PostsController::class], "admin.posts");
+        $router->post(ADMIN . "/delete", [AdminController::class, 'delete'], "admin.delete");
+    }
 
-photographers_routes : {
-    $router->get(ADMIN . "/locations", [LocationsController::class], "admin.location");
-    $router->any(ADMIN . "/locations/add", [LocationsController::class, 'add' ], "admin.location");
-    $router->any(ADMIN . "/locations/edit/:id", [LocationsController::class, 'edit' ], "admin.location");
-}
+    photographers_routes : {
+        $router->get(ADMIN . "/locations", [LocationsController::class], "admin.location");
+        $router->any(ADMIN . "/locations/add", [LocationsController::class, 'add' ], "admin.location");
+        $router->any(ADMIN . "/locations/edit/:id", [LocationsController::class, 'edit' ], "admin.location");
+    }
 
-gallery_routes : {
-    $router->any(ADMIN . "/gallery/add", [GalleryController::class, 'add' ], "admin.gallery-add");
-    $router->any(ADMIN . "/gallery/edit/:id", [GalleryController::class, 'edit' ], "admin.gallery-edit");
-    $router->any(ADMIN . "/gallery/albums/edit/:id", [AlbumsController::class, 'edit' ], "admin.album-edit");
-    $router->any(ADMIN . "/gallery/albums/add", [AlbumsController::class, 'add' ], "admin.album-add");
-    $router->any(ADMIN . "/gallery/watermark/:type/:filename", [GalleryController::class, 'watermark' ], "admin.watermarker");
-    $router->get(ADMIN . "/gallery", [GalleryController::class], "admin.gallery");
-    $router->get(ADMIN . "/gallery/albums", [AlbumsController::class], "admin.gallery.album");
-    $router->get(ADMIN . "/media-browser", [GalleryController::class, 'mediaBrowser' ], "admin.gallery-mediaBrowser");
-    $router->get(ADMIN . "/file-browser/:dirname", [GalleryController::class, 'fileBrowser' ], "admin.fileBrowser");
-    $router->post(ADMIN . "/deleteFile", [AdminController::class, 'deleteFile'], "admin.gallery-deletefile");
-}
+    gallery_routes : {
+        $router->any(ADMIN . "/gallery/add", [GalleryController::class, 'add' ], "admin.gallery-add");
+        $router->any(ADMIN . "/gallery/edit/:id", [GalleryController::class, 'edit' ], "admin.gallery-edit");
+        $router->any(ADMIN . "/gallery/albums/edit/:id", [AlbumsController::class, 'edit' ], "admin.album-edit");
+        $router->any(ADMIN . "/gallery/albums/add", [AlbumsController::class, 'add' ], "admin.album-add");
+        $router->any(ADMIN . "/gallery/watermark/:type/:filename", [GalleryController::class, 'watermark' ], "admin.watermarker");
+        $router->get(ADMIN . "/gallery", [GalleryController::class], "admin.gallery");
+        $router->get(ADMIN . "/gallery/albums", [AlbumsController::class], "admin.gallery.album");
+        $router->get(ADMIN . "/media-browser", [GalleryController::class, 'mediaBrowser' ], "admin.gallery-mediaBrowser");
+        $router->get(ADMIN . "/file-browser/:dirname", [GalleryController::class, 'fileBrowser' ], "admin.fileBrowser");
+        $router->post(ADMIN . "/deleteFile", [AdminController::class, 'deleteFile'], "admin.gallery-deletefile");
+    }
 
-users_routes : {
-    $router->get(ADMIN . "/users", [UsersController::class], "admin.users");
-    $router->get(ADMIN . "/users/permissions/:id", [UsersController::class, 'permissions' ], "admin.permissions");
-    $router->get(ADMIN . "/users/bugs", [UsersController::class, 'bugs' ], "admin.bugs");
-    $router->get(ADMIN . "/users/ideas", [UsersController::class, 'ideas' ], "admin.ideas");
-}
+    users_routes : {
+        $router->get(ADMIN . "/users", [UsersController::class], "admin.users");
+        $router->get(ADMIN . "/users/permissions/:id", [UsersController::class, 'permissions' ], "admin.permissions");
+        $router->get(ADMIN . "/users/bugs", [UsersController::class, 'bugs' ], "admin.bugs");
+        $router->get(ADMIN . "/users/ideas", [UsersController::class, 'ideas' ], "admin.ideas");
+    }
+};
--- config/routes/frontend.php
+++ config/routes/frontend.php
@@ -22,99 +22,101 @@ use Application\Controllers\SearchController;
 use Application\Controllers\StaticController;
 use Application\Controllers\UsersController;
 
-/** @var \Framework\Router\Router $router */
-$router->get("/", [HomeController::class], "home");
-$router->get("/home", [HomeController::class], "home");
 
-users_pages : {
-    $router->any("/login", [UsersController::class, 'login'], "users.login");
-    $router->any("/forgot", [UsersController::class, 'forgot'], "users.forgot");
-    $router->any("/reset/:id/:token", [UsersController::class, 'reset'], "users.reset");
-    $router->any("/sign", [UsersController::class, 'sign'], "users.sign");
-    $router->any("/settings/:token", [UsersController::class, 'edit'], "users.edit");
-    $router->get("/notifications/:id/:token", [NotificationsController::class], "notification.index");
-    $router->any("/edit-post/:id/:token", [PostsController::class, 'edit'], "posts.edit-article");
-    $router->any("/submit-photo", [PostsController::class, 'add'], "posts.add");
-    $router->get("/confirm/:id/:token", [UsersController::class, 'confirm'], "users.confirmation");
-    $router->get("/logout", [UsersController::class, 'logout'], "users.logout");
-    $router->get("/my-posts/:token", [PostsController::class, 'showPosts'], "posts.show-post");
-    $router->any("/my-posts/edit/:id/:token", [PostsController::class, 'edit'], "posts.edit");
-    $router->any("/my-posts/delete/:id/:token", [PostsController::class, 'delete'], "posts.delete");
-    $router->get("/my-followers/:token", [FollowingController::class, 'showFollowers'], "users.show-followers");
-    $router->get("/my-following/:token", [FollowingController::class, 'showFollowing'], "users.show-following");
-    $router->get('/my-collection/:token', [UsersController::class, 'collection'], 'saves.index');
-    $router->get('/my-notifications/:token', [UsersController::class, 'notification'], 'notification.index');
-    $router->get("/my-notifications/delete/:token", [NotificationsController::class, 'delete'], "notification.delete");
-    $router->get("/my-notifications/clear/:token", [NotificationsController::class, 'clear'], "notification.clear");
-}
+return function (Framework\Router\Router $router) {
+    $router->get("/", [HomeController::class], "home");
+    $router->get("/home", [HomeController::class], "home");
 
-community_routes : {
-    $router->get("/community", [CommunityController::class], "community.index");
-    $router->get("/community/search", [CommunityController::class, 'search'], "community.search");
-    $router->get("/community/photographers", [CommunityController::class, 'photographers'], "community.photographers");
-}
+    users_pages : {
+        $router->any("/login", [UsersController::class, 'login'], "users.login");
+        $router->any("/forgot", [UsersController::class, 'forgot'], "users.forgot");
+        $router->any("/reset/:id/:token", [UsersController::class, 'reset'], "users.reset");
+        $router->any("/sign", [UsersController::class, 'sign'], "users.sign");
+        $router->any("/settings/:token", [UsersController::class, 'edit'], "users.edit");
+        $router->get("/notifications/:id/:token", [NotificationsController::class], "notification.index");
+        $router->any("/edit-post/:id/:token", [PostsController::class, 'edit'], "posts.edit-article");
+        $router->any("/submit-photo", [PostsController::class, 'add'], "posts.add");
+        $router->get("/confirm/:id/:token", [UsersController::class, 'confirm'], "users.confirmation");
+        $router->get("/logout", [UsersController::class, 'logout'], "users.logout");
+        $router->get("/my-posts/:token", [PostsController::class, 'showPosts'], "posts.show-post");
+        $router->any("/my-posts/edit/:id/:token", [PostsController::class, 'edit'], "posts.edit");
+        $router->any("/my-posts/delete/:id/:token", [PostsController::class, 'delete'], "posts.delete");
+        $router->get("/my-followers/:token", [FollowingController::class, 'showFollowers'], "users.show-followers");
+        $router->get("/my-following/:token", [FollowingController::class, 'showFollowing'], "users.show-following");
+        $router->get('/my-collection/:token', [UsersController::class, 'collection'], 'saves.index');
+        $router->get('/my-notifications/:token', [UsersController::class, 'notification'], 'notification.index');
+        $router->get("/my-notifications/delete/:token", [NotificationsController::class, 'delete'], "notification.delete");
+        $router->get("/my-notifications/clear/:token", [NotificationsController::class, 'clear'], "notification.clear");
+    }
 
-posts_routes : {
-    $router->get("/posts", [PostsController::class, 'index'], "posts.index");
-    $router->get("/posts/post", [PostsController::class, 'post'], "posts.add");
-    $router->get("/posts/edit/:slug-:id", [PostsController::class, 'edit'], "posts.edit");
-    $router->get("/posts/:slug-:id", [PostsController::class, 'show'], "posts.show");
-    $router->get("/categories", [CategoriesController::class], "categories.index");
-    $router->get("/categories/:name-:id", [CategoriesController::class, 'show'], "categories.show");
-}
+    community_routes : {
+        $router->get("/community", [CommunityController::class], "community.index");
+        $router->get("/community/search", [CommunityController::class, 'search'], "community.search");
+        $router->get("/community/photographers", [CommunityController::class, 'photographers'], "community.photographers");
+    }
 
-gallery_routes : {
-    $router->get("/gallery", [GalleryController::class], "gallery.index");
-    $router->get("/gallery/slider", [GalleryController::class, 'slider'], "gallery.slider");
-    $router->get("/gallery/albums", [GalleryController::class, 'albums'], "gallery.albums");
-    $router->get("/gallery/albums/:slug-:id", [GalleryController::class, 'album_show'], "gallery.album_show");
-    $router->get("/gallery/:slug-:id", [GalleryController::class, 'show'], "gallery.show");
-}
+    posts_routes : {
+        $router->get("/posts", [PostsController::class, 'index'], "posts.index");
+        $router->get("/posts/post", [PostsController::class, 'post'], "posts.add");
+        $router->get("/posts/edit/:slug-:id", [PostsController::class, 'edit'], "posts.edit");
+        $router->get("/posts/:slug-:id", [PostsController::class, 'show'], "posts.show");
+        $router->get("/categories", [CategoriesController::class], "categories.index");
+        $router->get("/categories/:name-:id", [CategoriesController::class, 'show'], "categories.show");
+    }
 
-features_routes : {
-    $router->get("/likes/:type/:slug-:id", [LikesController::class], "likes");
-    $router->get("/likes/show/:type/:slug-:id", [LikesController::class, 'show'], "likes.show");
-    $router->get("/following/:name-:id", [FollowingController::class], "following");
-    $router->get("/download/:type/:name", [DownloadController::class], "download");
-    $router->get("/download/show/:type/:name", [DownloadController::class, 'show'], "download");
-    $router->get("/comments/:type/:slug-:id", [CommentsController::class, 'show'], "comments.show");
-    $router->post("/comments/:type/:slug-:id", [CommentsController::class, 'index'], "comments.add");
-    $router->post("/comments/edit/:id/:token", [CommentsController::class, 'edit'], "comments.edit");
-    $router->get("/comments/delete/:id/:token", [CommentsController::class, 'delete'], "comments.delete");
+    gallery_routes : {
+        $router->get("/gallery", [GalleryController::class], "gallery.index");
+        $router->get("/gallery/slider", [GalleryController::class, 'slider'], "gallery.slider");
+        $router->get("/gallery/albums", [GalleryController::class, 'albums'], "gallery.albums");
+        $router->get("/gallery/albums/:slug-:id", [GalleryController::class, 'album_show'], "gallery.album_show");
+        $router->get("/gallery/:slug-:id", [GalleryController::class, 'show'], "gallery.show");
+    }
 
-    $router->any("/contact", [ContactController::class], "contact.index");
-    $router->any("/report/:type/:slug-:id", [ReportsController::class], 'report.index');
-    $router->get("/about", [StaticController::class, 'about'], "static.about");
-    $router->get("/privacy", [StaticController::class, 'privacy'], "static.privacy");
-    $router->any("/app.offline", [StaticController::class, 'offline'], 'static.offline');
-    $router->any("/booking", [BookingController::class], 'reservation');
-    $router->get('/saves/:type/:slug-:id', [SavesController::class, 'add'], 'saves.add');
-    $router->get("/maps", [MapsController::class, 'show'], 'maps.show');
-    $router->get("/maps/photographers", [MapsController::class, 'photographers'], 'maps.photographers');
-}
+    features_routes : {
+        $router->get("/likes/:type/:slug-:id", [LikesController::class], "likes");
+        $router->get("/likes/show/:type/:slug-:id", [LikesController::class, 'show'], "likes.show");
+        $router->get("/following/:name-:id", [FollowingController::class], "following");
+        $router->get("/download/:type/:name", [DownloadController::class], "download");
+        $router->get("/download/show/:type/:name", [DownloadController::class, 'show'], "download");
+        $router->get("/comments/:type/:slug-:id", [CommentsController::class, 'show'], "comments.show");
+        $router->post("/comments/:type/:slug-:id", [CommentsController::class, 'index'], "comments.add");
+        $router->post("/comments/edit/:id/:token", [CommentsController::class, 'edit'], "comments.edit");
+        $router->get("/comments/delete/:id/:token", [CommentsController::class, 'delete'], "comments.delete");
 
-search_routes : {
-    $router->get("/search", [SearchController::class], "search.index");
-    $router->get("/search/:q", [SearchController::class], "search.index");
-}
+        $router->any("/contact", [ContactController::class], "contact.index");
+        $router->any("/report/:type/:slug-:id", [ReportsController::class], 'report.index');
+        $router->get("/about", [StaticController::class, 'about'], "static.about");
+        $router->get("/privacy", [StaticController::class, 'privacy'], "static.privacy");
+        $router->any("/app.offline", [StaticController::class, 'offline'], 'static.offline');
+        $router->any("/booking", [BookingController::class], 'reservation');
+        $router->get('/saves/:type/:slug-:id', [SavesController::class, 'add'], 'saves.add');
+        $router->get("/maps", [MapsController::class, 'show'], 'maps.show');
+        $router->get("/maps/photographers", [MapsController::class, 'photographers'], 'maps.photographers');
+    }
 
-$router->get("/feed", [RssController::class], "rss.index");
+    search_routes : {
+        $router->get("/search", [SearchController::class], "search.index");
+        $router->get("/search/:q", [SearchController::class], "search.index");
+    }
 
-//todo: replace with a strong API
-ajax_routes : {
-    $router->get("/ajax/posts", [AjaxController::class, 'posts'], "ajax.posts");
-    $router->get("/ajax/gallery", [AjaxController::class, 'gallery'], "ajax.gallery");
-    $router->get("/ajax/albums", [AjaxController::class, 'albums'], "ajax.albums");
-    $router->get("/ajax/categories", [AjaxController::class, 'categories'], "ajax.categories");
-    $router->get("/ajax/community", [AjaxController::class, 'community'], "ajax.community");
-    $router->get("/ajax/photographers", [AjaxController::class, 'photographers'], "ajax.photographers");
-    $router->get("/ajax/users_posts", [AjaxController::class, 'users_posts'], "ajax.users_posts");
-}
+    $router->get("/feed", [RssController::class], "rss.index");
 
-errors_routes : {
-    $router->get("/error/not-found", [ErrorController::class, 'e404'], "app.found");
-    $router->get("/error/internal", [ErrorController::class, 'e500'], "app.internal-server-error");
-    $router->get("/error/forbidden", [ErrorController::class, 'e403'], "app.forbidden");
-}
+    //todo: replace with a strong API
+    ajax_routes : {
+        $router->get("/ajax/posts", [AjaxController::class, 'posts'], "ajax.posts");
+        $router->get("/ajax/gallery", [AjaxController::class, 'gallery'], "ajax.gallery");
+        $router->get("/ajax/albums", [AjaxController::class, 'albums'], "ajax.albums");
+        $router->get("/ajax/categories", [AjaxController::class, 'categories'], "ajax.categories");
+        $router->get("/ajax/community", [AjaxController::class, 'community'], "ajax.community");
+        $router->get("/ajax/photographers", [AjaxController::class, 'photographers'], "ajax.photographers");
+        $router->get("/ajax/users_posts", [AjaxController::class, 'users_posts'], "ajax.users_posts");
+    }
 
-$router->get("/:user-:id", [UsersController::class, 'account'], "users.account");
+    errors_routes : {
+        $router->get("/error/not-found", [ErrorController::class, 'e404'], "app.found");
+        $router->get("/error/internal", [ErrorController::class, 'e500'], "app.internal-server-error");
+        $router->get("/error/forbidden", [ErrorController::class, 'e403'], "app.forbidden");
+    }
+
+    $router->get("/:user-:id", [UsersController::class, 'account'], "users.account");
+};
