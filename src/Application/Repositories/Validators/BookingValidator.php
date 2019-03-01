347e3d67e21af4158b2741335037fc1d3b97c4b6
bernard-ng <ngandubernard@gmail.com>
2019-03-01 23:34:41 +0200

    [Update] create components and delete repeted files

---
+++ src/Application/Repositories/Validators/BookingValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bernard-ng
+ * Date: 3/1/2019
+ * Time: 9:13 PM
+ */
+
+namespace Application\Repositories\Validators;
+
+
+class BookingValidator
+{
+
+}
