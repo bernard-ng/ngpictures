79029bef2f485742914b4fd36fef1c54f7d7ff9a
bernard-ng <ngandubernard@gmail.com>
2019-03-03 21:50:33 +0200

    [Update] remove old method in entity

--- src/Framework/Entities/Entity.php
+++ src/Framework/Entities/Entity.php
@@ -8,25 +8,4 @@ namespace Framework\Entities;
 class Entity
 {
 
-    /**
-     * @param $key
-     * @return null
-     */
-    public function __get($key)
-    {
-        $method = "get".ucfirst($key);
-        if (method_exists($this, $method)) {
-            $this->$key = $this->$method();
-            return $this->$key;
-        }
-        return null;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTime(): string
-    {
-        return date("D d M  Y", strtotime($this->date_created));
-    }
 }
