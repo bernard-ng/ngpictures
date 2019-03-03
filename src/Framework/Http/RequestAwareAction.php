5a82ce6eb0f4971c40a1ad2aaba7dcd03c9a1071
bernard-ng <ngandubernard@gmail.com>
2019-03-03 21:47:32 +0200

    [New] create Http abstraction

--- src/Framework/Http/RequestAwareAction.php
+++ src/Framework/Http/RequestAwareAction.php
@@ -1,15 +1,89 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Bernard-ng
- * Date: 2/28/2019
- * Time: 6:58 PM
- */
-
 namespace Framework\Http;
 
+use Application\Managers\PageManager;
 
-class RequestAwareAction
+/**
+ * Class RequestAwareAction
+ * @package Framework\Http
+ */
+trait RequestAwareAction
 {
+    /**
+     * l'instance de la request.
+     * @var ServerRequest;
+     */
+    private $currentRequest;
 
+
+    /**
+     * permet d'avoir access a la request.
+     * @return ServerRequest
+     */
+    public function getRequest()
+    {
+        $this->currentRequest = new ServerRequest();
+        return $this->currentRequest;
+    }
+
+
+    public function redirect($url = null, $ajax = false, int $code = 200)
+    {
+        $request = $this->getRequest();
+        if (empty($url)) {
+            if ($request->get('http.referer')) {
+                header("Location: {$request->get('http.referer')}", true, 301);
+                if ($this->getRequest()->ajax() && $ajax === true) {
+                    echo "/";
+                    exit();
+                }
+                exit();
+            } else {
+                $url = SITE_NAME;
+                if ($this->getRequest()->ajax() && $ajax === true) {
+                    echo $url;
+                    exit();
+                }
+                header("Location: {$url}", true, 301);
+                exit();
+            }
+        } else {
+            if ($this->getRequest()->ajax() && $ajax === true) {
+                echo $url;
+                exit();
+            }
+
+            $url = SITE_NAME . "/{$url}";
+            header("Location: {$url}", true, 301);
+            exit();
+        }
+    }
+
+
+    /**
+     * gestion de turbolinks
+     * @param string $name nom de la routes, location
+     */
+    public function turbolinksLocation(string $name)
+    {
+        PageManager::setUrl($name);
+        header("Turbolinks-Location: {$name}");
+    }
+
+
+    /**
+     * en cas d'erreur en ajax
+     * @param string $msg
+     * @param int|null $code
+     */
+    public function setFlash(string $msg, int $code = null)
+    {
+        if (is_null($code)) {
+            header('HTTP/1.1 500 Internal Server Error');
+        } else {
+            http_response_code($code);
+        }
+        echo $msg;
+        exit();
+    }
 }
--- src/Framework/Http/ServerRequest.php
+++ src/Framework/Http/ServerRequest.php
@@ -1,15 +1,155 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Bernard-ng
- * Date: 2/28/2019
- * Time: 6:57 PM
- */
-
 namespace Framework\Http;
 
+use Framework\Managers\Collection;
 
+/**
+ * Class ServerRequest
+ * @package Framework\Http
+ */
 class ServerRequest
 {
+    /**
+     * donnee recu en post
+     * @var array
+     */
+    private $post;
 
+    /**
+     * donnee recu en get
+     * @var array
+     */
+    private $get;
+
+    /**
+     * donnee du server
+     * @var array
+     */
+    private $server;
+
+    /**
+     * HttpRequest constructor.
+     */
+    public function __construct()
+    {
+        $this->post = $_POST;
+        $this->get = $_GET;
+        $this->files = $_FILES;
+        $this->server = $_SERVER;
+    }
+
+    /**
+     * @param string $method
+     * @return bool
+     */
+    public function methodAllowed(string $method)
+    {
+        $method = strtoupper($method);
+        if ($this->is('post') && $this->input('_method') == $method) {
+            return true;
+        } elseif ($this->is($method)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * renseigne la method de la request
+     * @param string $method
+     * @throws \InvalidArgumentException
+     * @return bool
+     */
+    public function is(string $method)
+    {
+        $method = strtoupper($method);
+        $expectedMethods = [
+            'POST', 'GET', 'PUT', 'DELETE', 'PATCH'
+        ];
+
+        if (isset($this->server['REQUEST_METHOD']) && !empty($this->server['REQUEST_METHOD'])) {
+            if (in_array($method, $expectedMethods)) {
+                return strtoupper($this->server['REQUEST_METHOD']) === $method;
+            } else {
+                throw new \OutOfRangeException(sprintf('the method "%s" not if expected request method', $method));
+            }
+        } else {
+            throw new \RuntimeException('undefined request method');
+        }
+    }
+
+    /**
+     * recupere les donnees du $_POST
+     * @param string $key
+     * @return Collection|null|mixed
+     */
+    public function input($key = null)
+    {
+        if ($key === null) {
+            return new Collection($this->post);
+        }
+        $data = new Collection($this->post);
+        return $data->get($key);
+    }
+
+    /**
+     * verifie la validite d'un token csrf
+     * @param string $token
+     * @return bool
+     */
+    public function validToken(string $token)
+    {
+        return $this->input('_token') == $token;
+    }
+
+    /**
+     * recupere les donnees du $_GET
+     * @param string|null $key
+     * @return Collection|null
+     */
+    public function query($key = null)
+    {
+        if ($key === null) {
+            return new Collection($this->get);
+        }
+        $data = new Collection($this->get);
+        return $data->get($key);
+    }
+
+
+    /**
+     * recupere les donnees du $_FILES
+     * @param string $key
+     * @return Collection|null
+     */
+    public function files($key = null)
+    {
+        if ($key === null) {
+            return new Collection($this->files);
+        }
+        $data = new Collection($this->files);
+        return $data->get($key);
+    }
+
+    /**
+     * renseigne si la request est en ajax.
+     * @return bool
+     */
+    public function ajax()
+    {
+        return ($this->get('http.x.requested.with') &&
+            strtolower($this->get('http.x.requested.with')) == 'xmlhttprequest') ? true : false;
+    }
+
+    /**
+     * recupere les donnees du $_SERVER
+     * @param string $key
+     * @return null
+     */
+    public function get(string $key)
+    {
+        $key = strtoupper(str_replace('.', '_', $key));
+        $data = new Collection($this->server);
+        return $data->get($key);
+    }
 }
