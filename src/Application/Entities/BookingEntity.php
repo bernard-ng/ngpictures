e96e2ef9ca580b2ba42ce69926ee94dd49d552ad
bernard-ng <ngandubernard@gmail.com>
2019-03-03 21:40:40 +0200

    [Update] fix collections, gallery and categories fetch bug

--- src/Application/Entities/BookingEntity.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Application\Entities;
-
-use Framework\Entities\Entity;
-
-class BookingEntity extends Entity
-{
-
-}
--- src/Application/Entities/LocationsEntity.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Application\Entities;
-
-use Framework\Entities\Entity;
-
-class LocationsEntity extends Entity
-{
-
-}
