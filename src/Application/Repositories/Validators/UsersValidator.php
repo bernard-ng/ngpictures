e1dd3a8062642f01f6a5f5f8109f2cf99401ae70
bernard-ng <ngandubernard@gmail.com>
2019-03-04 06:48:07 +0200

    [Update] update views for auth

---
+++ src/Application/Repositories/Validators/UsersValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bernard-ng
+ * Date: 3/3/2019
+ * Time: 9:58 PM
+ */
+
+namespace Application\Repositories\Validators;
+
+
+class UsersValidator
+{
+
+}
