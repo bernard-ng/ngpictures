7a1ff81335fee15d6352808afcb4aa33d575e637
bernard-ng <ngandubernard@gmail.com>
2019-03-09 18:58:41 +0200

    [Update] add auth extension

--- config/extensions.php
+++ config/extensions.php
@@ -4,4 +4,5 @@ return [
     \Twig_Extensions_Extension_Text::class,
     \Framework\Renderer\TwigRendererExtension::class,
     \Framework\Twig\RouterTwigExtension::class,
+    \Framework\Twig\AuthTwigExtension::class
 ];
