eca5fb85229980fd4d03f06e01f208b1e53d5017
bernard-ng <ngandubernard@gmail.com>
2019-03-09 18:52:43 +0200

    [New] create a factory for Event

---
+++ src/Framework/Events/EmitterFactory.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is a part of Ngpictures
+ * (c) Bernard Ngandu <ngandubernard@gmail.com>
+ *
+ */
+
+
+namespace Framework\Events;
+
+use League\Event\Emitter;
+use Psr\Container\ContainerInterface;
+
+
+/**
+ * Class EmiterFactory
+ * @package Framework\Events
+ */
+class EmitterFactory
+{
+
+    /**
+     * @param ContainerInterface $container
+     * @return Emitter
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $emitter = new Emitter();
+        (require(ROOT."/config/events.php"))($emitter, $container);
+        return $emitter;
+    }
+}
