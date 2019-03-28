9ba13a125be73ce676e95bc91ad10bbf677e35bd
bernard-ng <ngandubernard@gmail.com>
2019-03-28 03:39:19 +0200

    [Update] hotfix phinx config

--- phinx.php
+++ phinx.php
@@ -1,28 +1,35 @@
 <?php
 
-require __DIR__."/public/index.php";
+require __DIR__ . "/public/index.php";
 $connection = $container->get(PDO::class);
 
-return  [
+return [
     'paths' => [
-        'migrations' => __DIR__ ."/data/migrations",
-        'seeds' => __DIR__."/data/seeds",
+        'migrations' => [
+            __DIR__ . "/data/migrations/updates",
+            __DIR__ . "/data/migrations/create",
+        ],
+        'seeds' => [
+            __DIR__ . "/data/seeds",
+            __DIR__ . "/data/seeds/create",
+            __DIR__ . "/data/seeds/updates",
+        ],
     ],
 
     'environments' => [
-        'default_migration_table' => 'phinxlog',
+        'default_migration_table' => 'phinx_migrations_logs',
         'default_database' => 'development',
         'development' => [
-            'name' => 'ngpictures2',
+            'name' => $container->get('database.name'),
             'connection' => $connection,
             'collation' => 'utf8_unicode_ci',
             'charset' => 'utf8',
         ],
         'production' => [
-            'name' => 'larytech_ngbd',
+            'name' => $container->get('database.name'),
             'connection' => $connection,
             'collation' => 'utf8_unicode_ci',
             'charset' => 'utf8',
-        ]
-    ]
+        ],
+    ],
 ];
